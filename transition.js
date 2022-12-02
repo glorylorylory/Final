0051a081ccc41eb5a6ce9307cb73ddea13ae5cf9
glorylorylory <114446170+glorylorylory@users.noreply.github.com>
2022-12-02 07:45:52 -0800

    dbgnfsyjrhtzgfcb

--- transition.js
+++ transition.js
@@ -1,22 +1,11 @@
-// var tr = document.getElementsByClassName('menu'); // Assumes element with id='button'
-
-// tr.onclick = function() {
-//     var div = document.getElementById('menu');
-//     if (div.style.display !== 'none') {
-//         div.style.display = 'none';
-//     }
-//     else {
-//         div.style.display = 'block';
-//     }
-// };
-
-// $(".menu").click(function() { 
-//     // assumes element with id='button'
-//     $("#menu").toggle();
-// });
-
 jQuery(document).ready(function(){
     jQuery('.hideshow').on('click', function(event) {        
         jQuery('#menu').toggle('show');
+    });
+});
+
+jQuery(document).ready(function(){
+    jQuery('.hideshow2').on('click', function(event) {        
+        jQuery('#fooditems').toggle('show');
     });
 });
