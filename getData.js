d9e7b2bf2cfc0f96284cbd6b9c5be1d3196376d2
glorylorylory <114446170+glorylorylory@users.noreply.github.com>
2022-12-01 23:34:58 -0800

    update

---
+++ getData.js
@@ -0,0 +1,47 @@
+// $.getJSON('restaurants.json', function(data) {
+    
+
+//     data['restaurants'].forEach(function (d) {
+//         $('#restaurants').append(d['name'] + ' - ' + d['type'] + d['delivery'] + d['distance'] + d['time'] +'<br>') 
+//     });
+// });
+
+
+
+    // $.getJSON("restaurants.json", function (data) {
+    //     var restaurants = '';
+
+    //     $.each(data, function (_key, value) {
+    //         restaurants += '<table>';
+    //         restaurants += '<tr>';
+    //         restaurant += '<td>' +
+    //             value.name + '</td>';
+            
+    //         restaurants += '<td>' +
+    //             value.type + '</td>';
+    //     });
+
+    //     $('#restaurants').append(restaurants);
+    // });
+
+fetch('restaurants.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+function appendData(data) {
+    var mainContainer = document.getElementById("restaurants");
+
+    for (var i = 0; i <data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].name + ' ' + data[i].type;
+        mainContainer.appendChild(div)
+    }
+}
+
