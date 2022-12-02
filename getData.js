d8763cd78074c32d9cc7fc701d8760a24a54d2d9
glorylorylory <114446170+glorylorylory@users.noreply.github.com>
2022-12-02 08:02:24 -0800

    dshyjd

--- getData.js
+++ getData.js
@@ -21,6 +21,18 @@
         $('#restaurants').append(restaurants);
     });
 
+    jQuery(document).ready(function(){
+        jQuery('.hideshow').on('click', function(event) {        
+            jQuery('#menu').toggle('show');
+        });
+    });
+    
+    jQuery(document).ready(function(){
+        jQuery('.hideshow2').on('click', function(event) {        
+            jQuery('#fooditems').toggle('show');
+        });
+    });
+
 // fetch('restaurants.json')
 //     .then(function (response) {
 //         return response.json();
